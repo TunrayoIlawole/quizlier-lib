584df4d432cc113f438349e9a6ef1e3c9e85c8c5
Motunrayo Ilawole <motunrayo.ilawole@flutterwavego.com>
2024-06-11 14:22:41 +0100

    update columns

--- common/src/main/java/com/quizlier/common/entity/Option.java
+++ common/src/main/java/com/quizlier/common/entity/Option.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -28,6 +29,7 @@ public class Option {
     private Boolean isCorrect;
     
     @ManyToOne
+    @JoinColumn(name = "questionid")
     private Question question;
     
 	public Long getId() {
--- common/src/main/java/com/quizlier/common/entity/Question.java
+++ common/src/main/java/com/quizlier/common/entity/Question.java
@@ -9,6 +9,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -25,6 +26,7 @@ public class Question {
     private String question;
     
     @ManyToOne
+    @JoinColumn(name = "categoryid")
     private Category category;
     
 	public Long getId() {

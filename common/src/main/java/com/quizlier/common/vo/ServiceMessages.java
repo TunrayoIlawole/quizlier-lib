48baf343646d5e08f575e61952fdd03bab205f8b
Motunrayo Ilawole <tunrayoilawole99@gmail.com>
2024-08-22 21:50:09 +0100

    update response messages

--- common/src/main/java/com/quizlier/common/vo/ServiceMessages.java
+++ common/src/main/java/com/quizlier/common/vo/ServiceMessages.java
@@ -9,6 +9,17 @@ public class ServiceMessages {
 	public static final String INVALID_PASSWORD = "Password cannot be empty. Please enter a value";
 	public static final String CREDENTIALS_MISMATCH = "The email and password provided do not match. Please try again";
 	public static final String INVALID_USER = "That email address does not exist in our database. Please sign up";
+	public static final String MAXIMUM_OPTIONS = "Maximum number of options for question exceeded";
+	public static final String DUPLICATE_OPTIONS = "Option already exists for this question";
+	public static final String NO_CORRECT_ANSWER = "Question does not have a correct answer set";
+
+	public static String invalidEntity(String entity, String id) {
+		return String.format("'%s' with id '%s' does not exist", entity, id);
+	}
+
+	public static String duplicateEntity(String entity) {
+		return String.format("'%s' already exists", entity);
+	}
 	
 	public static final String GENERAL_ERROR_MESSAGE = "Request processing error";
 	public static final String SUCCESS_RESPONSE = "Request processed successfully";
